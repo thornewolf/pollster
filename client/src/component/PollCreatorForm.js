24a5bd4b539a787633141e82c739cb4a10899ad8
n-wes <n-wes@DESKTOP-6HC32TO.localdomain>
2020-06-17 18:41:47 -0700

    added max length to all textfields and min length to title. filter out empty textfields on form submission

--- client/src/component/PollCreatorForm.js
+++ client/src/component/PollCreatorForm.js
@@ -9,6 +9,7 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import axios from 'axios'
 
+const MAX_TEXTFIELD_LENGTH = 280
 
 const useStyles = theme => ({
     fab: {
@@ -23,7 +24,14 @@ const useStyles = theme => ({
 function PollAnswer(props) {
     return (
         <div style={{ paddingBottom: 10 }}>
-        <TextField key={props.label} label={props.label} value={props.value} variant="standard" onChange={props.handleChange} />
+        <TextField
+            key={props.label}
+            label={props.label}
+            value={props.value}
+            variant="standard"
+            inputProps={{ maxLength: MAX_TEXTFIELD_LENGTH }}
+            onChange={props.handleChange}
+        />
         </div>
     )
 }
@@ -33,7 +41,7 @@ class PollCreatorForm extends React.Component {
         super(props)
         this.state = {
             answerCount: 1,
-            fields: [],
+            fields: ['', ''],
             isFormValid: false,
         }
         this.handleChange = this.handleChange.bind(this)
@@ -51,12 +59,15 @@ class PollCreatorForm extends React.Component {
     handleChange(i, event) {
         let fields = [...this.state.fields]
         fields[i] = event.target.value
-        let areFieldsValid = fields.map(field => field.length < 280)
+
+        let areFieldsValid = fields.map(field => field.length < MAX_TEXTFIELD_LENGTH)
+
         this.setState({
             fields: fields,
             isFormValid: areFieldsValid.reduce((a, b) => a && b),
             areFieldsValid: areFieldsValid,
         })
+
         if (i === this.state.answerCount) {
             this.setState({
                 answerCount: i+1
@@ -66,29 +77,33 @@ class PollCreatorForm extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault()
-        if (!this.state.isFormValid) {
-            console.log("invalid")
+        if (!this.state.isFormValid || this.state.fields[0].length === 0) {
             return false
         }
         const submission = [...this.state.fields]
         const question = submission[0]
-        const answers = submission.slice(1,submission.length).map(answer => ({body: answer}))
+        const answers = submission.slice(1,submission.length)
+            .filter(answer => answer.length > 0 && answer.length <= MAX_TEXTFIELD_LENGTH)
+            .map(answer => ({body: answer}))
         const payload = {
             question: question,
             answers: answers
         }
+
+        console.log("Sending Payload: " + payload)
         axios.post('http://localhost:5000/polls/add', payload).then(res => console.log(res.data))
         this.resetForm() 
     }
 
     render() {
         var answers = [];
-        console.log(this.state.fields)
+
         for (let i = 1; i < this.state.answerCount+1; i++) {
             answers.push((
             <PollAnswer key={i} label={"Option "+(i)} value={this.state.fields[i]} handleChange={this.handleChange.bind(this, i)} />
             ))
         } 
+
         return (
             <Paper>
                 <ExpansionPanel >
